6b5a38957620ff3b3688ca1457bda3d831877f0d
Ben <bensiso1992@gmail.com>
2019-10-10 14:53:15 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -122,7 +122,7 @@ function chosenCommunity(communityName) {
 	    	
 		} else {
 	    $(window).on('load',function () {
-		   document.getElementById('newTabCounter').innerHTML = 'JOIN US <img src="./heartPink.png" style="width: 25px; height: 25px;margin-left: 15px;"> ';
+		   document.getElementById('newTabCounter').innerHTML = 'JOIN U1S <img src="./heartPink.png" style="width: 25px; height: 25px;margin-left: 15px;"> ';
 		   document.getElementById("newTabCounter").href="https://chrome.google.com/webstore/detail/tabfundme/naphccalgkhlohfgnjbjmnbibojlmbea";
        	   $('.hover_bkgr_fricc3').show();
        	   console.log("no_u_no_com");
